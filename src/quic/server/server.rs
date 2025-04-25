5d2d3c9f295043a8d08a87f0ec74e4f7c4ebb7f6
Bronstein0x113c1c3 <tuananhsmart123456789@gmail.com>
2025-04-25 09:19:25 +0700

    quic with super fine control :)))))))))))))), ultimate version

--- src/quic/server/server.rs
+++ src/quic/server/server.rs
@@ -1,10 +1,10 @@
 use std::{
     io::Read,
-    net::{SocketAddr, SocketAddrV4},
+    net::{SocketAddr, SocketAddrV4}, time::Duration,
 };
 
 use rustls::crypto::aws_lc_rs::default_provider;
-use tokio::io::AsyncWriteExt;
+use tokio::{io::AsyncWriteExt, time::sleep};
 
 mod server_util;
 #[tokio::main]
@@ -13,7 +13,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>
     default_provider().install_default().unwrap();
     //decompress the sound chunk
     let file = std::io::BufReader::new(
-        std::fs::File::open("./list_songs/rightforyou_instrumental.mp3").unwrap(),
+        std::fs::File::open("./list_songs/ThatllBeTheDay-ShawnMendes-16714907_hq.mp3").unwrap(),
     );
     // std::io::pipe()
     let mut source = rodio::Decoder::new(file).unwrap();
@@ -33,16 +33,33 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>
     let (endpoint, _) = server_util::make_server_endpoint(addr).unwrap();
     let incoming = endpoint.accept().await.unwrap();
     let mut conn = incoming.await.unwrap();
-    let mut uni = conn.open_uni().await.unwrap();
+    let mut bi = conn.open_bi().await.unwrap();
     // uni.
-    let mut iter = bytes.chunks(8820);
-    while let Some(chunk) = iter.next() {
-        // uni.stopped()
-        uni.write(chunk).await;
-        uni.flush();
+    // implement the ack!!!!
+    let mut iter = bytes.chunks(44100);
+    while let Some(chunk) = iter.next(){
+
+        bi.0.write_all(chunk).await;
+        
+        sleep(Duration::from_millis(200)).await;
+        bi.0.flush().await;
+        println!("sended!!!");
+        // sleep(Duration::from_millis(10)).await;
+        // let mut ack_buffer = [0u8;1];
+        // bi.1.read_exact(&mut ack_buffer).await;
+        // println!("ack received!!!");
+
     }
-    uni.finish();
-    uni.stopped().await;
+    // while let Some(chunk) = iter.next() {
+    //     // uni.stopped()
+    //     uni.write(chunk).await;
+    //     sleep(Duration::from_millis(1)).await;
+    //     uni.flush();
+    // }
+    
+    bi.0.finish();
+    bi.0.shutdown().await;
+    // uni.stopped().await;
     //send the sound to the client...
     Ok(())
 }
